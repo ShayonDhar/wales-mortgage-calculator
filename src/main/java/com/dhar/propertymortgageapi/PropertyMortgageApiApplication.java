78c5f696182a2049b2bd3d8c1113ec295661ece9
Shayon Dhar <shayondhar@yahoo.com>
2026-03-07 18:17:43 +0000

    Initialise Project

---
+++ src/main/java/com/dhar/propertymortgageapi/PropertyMortgageApiApplication.java
@@ -0,0 +1,13 @@
+package com.dhar.propertymortgageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyMortgageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyMortgageApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dhar/propertymortgageapi/PropertyMortgageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhar.propertymortgageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyMortgageApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
